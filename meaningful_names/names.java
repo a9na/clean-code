73e779a6388f49712f518cd9a544f7110fa514f2
Ana Novković <anaiscoding@gmail.com>
2024-04-28 18:49:15 +0200

    Update names.java

--- meaningful_names/names.java
+++ meaningful_names/names.java
@@ -17,3 +17,22 @@ if (x[0] == 4)
 list1.add(x);
 return list1;
 }
+
+// improved:
+public List<int[]> getFlaggedCells() {
+List<int[]> flaggedCells = new ArrayList<int[]>();
+for (int[] cell : gameBoard)
+if (cell[STATUS_VALUE] == FLAGGED)
+flaggedCells.add(cell);
+return flaggedCells;
+}
+
+// even better:
+public List<Cell> getFlaggedCells() {
+List<Cell> flaggedCells = new ArrayList<Cell>();
+for (Cell cell : gameBoard)
+if (cell.isFlagged())
+flaggedCells.add(cell);
+return flaggedCells;
+}
+
