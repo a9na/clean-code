b73357deefd67d44dcf7b6b57a9da60b65786aae
Ana Novković <anaiscoding@gmail.com>
2024-04-28 18:55:03 +0200

    Update naming.java

--- chapter2/naming.java
+++ chapter2/naming.java
@@ -35,3 +35,11 @@ if (cell.isFlagged())
 flaggedCells.add(cell);
 return flaggedCells;
 }
+
+/**
+In the absence of specific conventions, the variable moneyAmount is
+indistinguishable from money, customerInfo is indistinguishable from customer,
+accountData is indistinguishable from account, and theMessage is
+indistinguishable from message. Distinguish names in such a way that the reader
+knows what the differences offer.
+**/
